f34f61fac263d056875226f43f89ab717351fb26
guihroch <guihroch@gmail.com>
2023-11-29 11:33:04 -0300

    first commit

---
+++ app/src/main/java/com/example/movieapp/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.movieapp
+
+import android.content.Intent
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.View
+import com.example.movieapp.activities.CadastroPage
+import com.example.movieapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        window.statusBarColor = Color.parseColor("#FF000000")
+
+        binding.toCadastroPage.setOnClickListener {
+            val intent = Intent(this, CadastroPage::class.java)
+            startActivity(intent)
+        }
+
+        binding.buttonLogin.setOnClickListener {
+        autenticacaoUsuario()
+        }
+    }
+    private fun autenticacaoUsuario(){
+        binding.buttonLogin.visibility = View.GONE
+        binding.containerProgressbarLogin.visibility = View.VISIBLE
+        Handler(Looper.getMainLooper()).postDelayed({
+            binding.buttonLogin.visibility = View.VISIBLE
+            binding.containerProgressbarLogin.visibility = View.GONE
+        }, 2000)
+    }
+}
---
+++ app/src/main/java/com/example/movieapp/activities/CadastroPage.kt
@@ -0,0 +1,77 @@
+package com.example.movieapp.activities
+
+import android.content.Intent
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.Toast
+import com.example.movieapp.MainActivity
+import com.example.movieapp.R
+import com.example.movieapp.databinding.ActivityCadastroPageBinding
+
+class CadastroPage : AppCompatActivity() {
+    private lateinit var binding:ActivityCadastroPageBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityCadastroPageBinding.inflate(layoutInflater)
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        window.statusBarColor = Color.parseColor("#FF000000")
+
+        binding.buttonContinuar.setOnClickListener {
+           logicaProgressBarCadastro()
+        }
+
+        binding.buttonCadastrar.setOnClickListener {
+            //Finalizar com toast customizada
+            //Retornar automaticamente para a loginPage
+            binding.containerProgressbar.visibility = View.VISIBLE
+            binding.buttonCadastrar.visibility = View.GONE
+            Handler(Looper.getMainLooper()).postDelayed({
+                binding.containerProgressbar.visibility = View.GONE
+                binding.buttonCadastrar.visibility = View.VISIBLE
+                customToastError()
+            },2000)
+        }
+
+
+    }
+
+    private fun logicaProgressBarCadastro(){
+        //Corrigir o Nome da função
+        binding.textFinalizeSeuCadastro.visibility = View.VISIBLE
+        binding.textInformeEmail.visibility = View.INVISIBLE
+        binding.textSenha.visibility = View.VISIBLE
+        binding.editTextSenha.visibility = View.VISIBLE
+        binding.buttonContinuar.visibility = View.GONE
+        binding.buttonCadastrar.visibility = View.VISIBLE
+
+
+    }
+    private fun toLoginPage(){
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+
+    fun toastSucesso(){
+        //Fazer toast erro
+        val view = LayoutInflater.from(this).inflate(R.layout.toast_customizada_sucesso, null, false)
+        val toastSucesso = Toast(this)
+        toastSucesso.view = view
+        toastSucesso.duration = Toast.LENGTH_SHORT
+        toastSucesso.show()
+    }
+
+    fun customToastError(){
+        val view = LayoutInflater.from(this).inflate(R.layout.toast_customizada_error, null, false)
+        val toastErro = Toast(this)
+        toastErro.view = view
+        toastErro.duration = Toast.LENGTH_SHORT
+        toastErro.show()
+    }
+
+}
